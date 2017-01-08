cba2048cb9034be12a321879ac797541ada1b535
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-01-08 15:57:59 +0100

    Remove the AppBundle and its associate elements (paths, statements, ..)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+
+            //Our new bundles go below
             new NBGraphics\CoreBundle\NBGraphicsCoreBundle(),
         ];
 
