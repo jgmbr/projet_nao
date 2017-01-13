ce0c239142793e205da699fb28dca26aae92f92c
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-01-13 14:33:27 +0100

    Add some simple constraints to the observation form

--- src/NBGraphics/CoreBundle/Form/ObservationFormType.php
+++ src/NBGraphics/CoreBundle/Form/ObservationFormType.php
@@ -11,6 +11,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class ObservationFormType extends AbstractType
 {
@@ -24,27 +27,47 @@ class ObservationFormType extends AbstractType
                     'Un individu' => 'individu',
                     'Un couple' => 'couple',
                     'Un groupe' => 'groupe'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous devez renseigner ce champ.'
+                    ]),
+                    new NotNull([
+                        'message' => 'Vous devez renseigner ce champ.'
+                    ])
                 ]
             ])
             ->add('dateAt', DateType::class, [
                 'label' => "Date de l'observation : ",
                 //Modifier l'implémentation de la date après
                 'widget' => 'choice',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous devez renseigner la date de l\'observation.'
+                    ]),
+                    // A partir de quand peut-on renseigner une observation ?
+                ]
             ])
             ->add('hourAt', TimeType::class, [
                 'label' => "Heure de l'observation : ",
-                'input' => 'datetime'
+                'input' => 'datetime',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous devez renseigner l\'horaire de l\'observation.'
+                    ])
+                ]
             ])
             //Regarder pour utiliser un ClassType spécial
             ->add('image', FileType::class, [
                 'label' => "Ajouter votre photo : "
+                // Ajouter des contraintes spécifiques aux photos.
             ])
             //Faire un ChoiceType avec tous les départements
             ->add('departement', ChoiceType::class, [
                 'label' => 'Veuillez compléter votre département/adresse',
                 'choices' => [
-                    '75 Paris' => '75',
-                    "95 Val d'Oise" => '95'
+                    '75 Paris' => 75,
+                    "95 Val d'Oise" => 95
                 ]
             ])
             // Coordonnées GPS :
@@ -56,7 +79,16 @@ class ObservationFormType extends AbstractType
             ])
 
             ->add('comment', TextareaType::class, [
-                'label' => "Veuillez compléter votre observation"
+                'label' => "Veuillez compléter votre observation",
+                'constraints' => [
+                    New NotBlank([
+                        'message' => "Vous ne pouvez pas soumettre votre observation sans un message d'explication."
+                    ]),
+                    new GreaterThanOrEqual([
+                        'value' => 50,
+                        'message' => 'Votre observation doit au moins comprendre 50 caractères.'
+                    ])
+                ]
             ])
         ;
     }
