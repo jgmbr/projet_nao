0482457470d673b0c16eccc8aef4f04315b7a99a
JGambier <justine.gambier.1@gmail.com>
2017-02-08 22:22:20 +0100

    Fix load fixtures with test already exists

--- src/NBGraphics/CoreBundle/DataFixtures/ORM/LoadStatus.php
+++ src/NBGraphics/CoreBundle/DataFixtures/ORM/LoadStatus.php
@@ -10,23 +10,29 @@ class LoadStatus implements FixtureInterface
 {
 	public function load(ObjectManager $manager)
 	{
-        $status1 = new Status();
-        $status1->setName('En attente');
-        $status1->setIcon('fa-clock-o');
-        $status1->setRole('DEFAULT');
-		$manager->persist($status1);
+        if (!is_object($manager->getRepository(Status::class)->findOneByRole('DEFAULT'))) {
+            $status1 = new Status();
+            $status1->setName('En attente');
+            $status1->setIcon('fa-clock-o');
+            $status1->setRole('DEFAULT');
+            $manager->persist($status1);
+        }
 
-        $status2 = new Status();
-        $status2->setName('Validée');
-        $status2->setIcon('fa-check');
-        $status2->setRole('VALIDED');
-        $manager->persist($status2);
+        if (!is_object($manager->getRepository(Status::class)->findOneByRole('VALIDED'))) {
+            $status2 = new Status();
+            $status2->setName('Validée');
+            $status2->setIcon('fa-check');
+            $status2->setRole('VALIDED');
+            $manager->persist($status2);
+        }
 
-        $status3 = new Status();
-        $status3->setName('Refusée');
-        $status3->setIcon('fa-times');
-        $status3->setRole('REFUSED');
-        $manager->persist($status3);
+        if (!is_object($manager->getRepository(Status::class)->findOneByRole('REFUSED'))) {
+            $status3 = new Status();
+            $status3->setName('Refusée');
+            $status3->setIcon('fa-times');
+            $status3->setRole('REFUSED');
+            $manager->persist($status3);
+        }
 
 		$manager->flush();
 	}
--- src/NBGraphics/NewsBundle/DataFixtures/ORM/LoadState.php
+++ src/NBGraphics/NewsBundle/DataFixtures/ORM/LoadState.php
@@ -10,15 +10,19 @@ class LoadState implements FixtureInterface
 {
 	public function load(ObjectManager $manager)
 	{
-        $status1 = new State();
-        $status1->setName('Brouillon');
-        $status1->setRole('DEFAULT');
-		$manager->persist($status1);
+        if (!is_object($manager->getRepository(State::class)->findOneByRole('DEFAULT'))) {
+            $status1 = new State();
+            $status1->setName('Brouillon');
+            $status1->setRole('DEFAULT');
+            $manager->persist($status1);
+        }
 
-        $status2 = new State();
-        $status2->setName('Publié');
-        $status2->setRole('PUBLISH');
-        $manager->persist($status2);
+        if (!is_object($manager->getRepository(State::class)->findOneByRole('PUBLISH'))) {
+            $status2 = new State();
+            $status2->setName('Publié');
+            $status2->setRole('PUBLISH');
+            $manager->persist($status2);
+        }
 
 		$manager->flush();
 	}
