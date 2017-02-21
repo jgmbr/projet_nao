f75cad350054f206970b499038be49df6ad2ff54
JGambier <justine.gambier.1@gmail.com>
2017-02-21 20:59:17 +0100

    Add constraints registration form

--- src/NBGraphics/UserBundle/Form/RegistrationType.php
+++ src/NBGraphics/UserBundle/Form/RegistrationType.php
@@ -11,6 +11,9 @@ namespace NBGraphics\UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Length;
@@ -21,9 +24,44 @@ class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildForm($builder, $options);
-
         $builder
+            ->add('email',EmailType::class, array(
+                'label' => 'form.email',
+                'translation_domain' => 'FOSUserBundle',
+                'required' => true,
+            ))
+            ->add('username', null, array(
+                'label' => 'form.username',
+                'translation_domain' => 'FOSUserBundle',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez compléter votre nom d\'utilisateur'
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Nombre de caractères minimum requis : 5',
+                        'max' => 25,
+                        'maxMessage' => 'Nombre de caractères maximum requis : 25',
+                    ])
+                ]
+            ))
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+                'required' => true,
+                'constraints' => [
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit faire au moins 8 caractères',
+                        'max' => 255,
+                        'maxMessage' => 'Votre mot de passe est trop long',
+                    ])
+                ]
+            ))
             ->add('firstname', TextType::class, array(
                 'label' => 'form.firstname',
                 'translation_domain' => 'FOSUserBundle',
