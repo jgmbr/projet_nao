7f00031fdf22a07534b8dcb0bdab5f4fa21dccad
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-03-01 10:37:24 +0100

    Finish the overlay. Start adding it to the other pages of the web site

--- web/assets/js/imageOverlay.js
+++ web/assets/js/imageOverlay.js
@@ -3,19 +3,47 @@
 //
 
 
-// Adding new Variables
-$imgToShow = $('<img>');
-
 // Existing Variables
-$tdImage = $('#obs-image');
-$anchor = $tdImage.find('a');
-href = $anchor.attr('href');
-$img = $anchor.find('img');
+var $tdImage = $('#obs-image');
+var $anchor = $tdImage.find('a');
+var href = $anchor.attr('href');
+var $img = $anchor.find('img');
+var alt = $img.attr('alt');
 
 
+// Create the overlay and its linked elements
+var $overlay = $('<div id="overlay"></div>');
+var $overlayImgContainer = $('<div id="overlayImgContainer"></div>');
+var $closeThis = $('<p>Fermer la fenêtre</p>')
+var $imgToShow = $('<img src="" alt="">');
 
+
+// Append the elements to the container
+$overlayImgContainer.append($imgToShow);
+$overlayImgContainer.append($closeThis);
+$overlay.append($overlayImgContainer);
+$('.main-wrapper').append($overlay);
+
+
+// The click event which shows the overlay
 $anchor.click(function(event) {
     event.preventDefault();
-    console.log('foo');
-    
+
+    // Fill in the image information
+    $imgToShow.attr('src', href);
+    $imgToShow.attr('alt', alt);
+
+    // Show the overlay & its picture
+    $overlay.fadeIn('fast');
+});
+
+
+$closeThis.click(function() {
+    $overlay.fadeOut('fast');
+});
+
+$(document).keydown(function(event) {
+    if (event.which === 27) {
+        $overlay.fadeOut('fast');
+    }
 });
