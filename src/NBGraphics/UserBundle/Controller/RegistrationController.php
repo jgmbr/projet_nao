68a6a495ca5b611fd551c3c63712a4e70f20eabc
JGambier <justine.gambier.1@gmail.com>
2017-03-09 15:34:40 +0100

    Fix registration process

--- src/NBGraphics/UserBundle/Controller/RegistrationController.php
+++ src/NBGraphics/UserBundle/Controller/RegistrationController.php
@@ -22,6 +22,11 @@ use FOS\UserBundle\Controller\RegistrationController as BaseController;
 
 class RegistrationController extends BaseController
 {
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
     public function registerAction(Request $request)
     {
         /** @var $formFactory FactoryInterface */
@@ -55,16 +60,9 @@ class RegistrationController extends BaseController
 
                 if (null === $response = $event->getResponse()) {
 
-                    $url = $this->generateUrl('admin_page');
+                    $url = $this->generateUrl('fos_user_registration_confirmed');
 
-                    $mailer = $this->get('app.email.registration.form');
-
-                    $send = $mailer->sendEmail($user);
-
-                    if ($send) {
-                        $response = new RedirectResponse($url);
-                    }
-
+                    $response = new RedirectResponse($url);
                 }
 
                 $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
@@ -80,8 +78,99 @@ class RegistrationController extends BaseController
             }
         }
 
-        return $this->render('FOSUserBundle:Registration:register.html.twig', array(
+        return $this->render('@FOSUser/Registration/register.html.twig', array(
             'form' => $form->createView(),
         ));
+    }
+
+    /**
+     * Tell the user to check his email provider.
+     */
+    public function checkEmailAction()
+    {
+        $email = $this->get('session')->get('fos_user_send_confirmation_email/email');
+
+        if (empty($email)) {
+            return new RedirectResponse($this->get('router')->generate('fos_user_registration_register'));
+        }
+
+        $this->get('session')->remove('fos_user_send_confirmation_email/email');
+        $user = $this->get('fos_user.user_manager')->findUserByEmail($email);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
+        }
+
+        return $this->render('@FOSUser/Registration/check_email.html.twig', array(
+            'user' => $user,
+        ));
+    }
+
+    /**
+     * Receive the confirmation token from user email provider, login the user.
+     *
+     * @param Request $request
+     * @param string  $token
+     *
+     * @return Response
+     */
+    public function confirmAction(Request $request, $token)
+    {
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+
+        $user = $userManager->findUserByConfirmationToken($token);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
+        }
+
+        /** @var $dispatcher EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+
+        $user->setConfirmationToken(null);
+        $user->setEnabled(true);
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRM, $event);
+
+        $userManager->updateUser($user);
+
+        if (null === $response = $event->getResponse()) {
+            $url = $this->generateUrl('fos_user_registration_confirmed');
+            $response = new RedirectResponse($url);
+        }
+
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRMED, new FilterUserResponseEvent($user, $request, $response));
+
+        return $response;
+    }
+
+    /**
+     * Tell the user his account is now confirmed.
+     */
+    public function confirmedAction()
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->render('@FOSUser/Registration/confirmed.html.twig', array(
+            'user' => $user,
+            'targetUrl' => $this->getTargetUrlFromSession(),
+        ));
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getTargetUrlFromSession()
+    {
+        $key = sprintf('_security.%s.target_path', $this->get('security.token_storage')->getToken()->getProviderKey());
+
+        if ($this->get('session')->has($key)) {
+            return $this->get('session')->get($key);
+        }
     }
 }
