63ccb89e9dcdcae26b8a81b3bcd4bac905f94e55
JGambier <justine.gambier.1@gmail.com>
2017-03-10 10:41:58 +0100

    Create landing page for integration

---
+++ src/NBGraphics/FrontSiteBundle/Controller/LandingController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace NBGraphics\FrontSiteBundle\Controller;
+
+use NBGraphics\CoreBundle\Entity\Newsletter;
+use NBGraphics\CoreBundle\Form\NewsletterFormType;
+use NBGraphics\SeoBundle\Entity\Seo;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class LandingController extends Controller
+{
+    /**
+     * @Route("/observation-des-oiseaux",
+     *     name="nb_graphics_front_site_landing_a",
+     *     defaults={
+     *          "seo": true,
+     *          "page": "Page Landing A"
+     *     }
+     *  )
+     */
+    public function landingAPageAction()
+    {
+        return $this->render('@NBGraphicsFrontSite/landing/a.html.twig');
+    }
+
+    /**
+     * @Route("/observer-des-oiseaux",
+     *     name="nb_graphics_front_site_landing_b",
+     *     defaults={
+     *          "seo": true,
+     *          "page": "Page Landing B"
+     *     }
+     *  )
+     */
+    public function landingBPageAction()
+    {
+        return $this->render('@NBGraphicsFrontSite/landing/b.html.twig');
+    }
+}
